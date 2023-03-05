910168ca1de67e605868b6f3b0180cfcc6ae652a
Ritesh <riteshsaini331@gmail.com>
2023-03-05 22:15:56 +0530

    Home Screen Buy Now Fix

--- public/static/script.js
+++ public/static/script.js
@@ -1,5 +1,4 @@
 let items = document.getElementById("products");
-let load_more = document.getElementById("load_more");
 
 let pop_up = document.getElementById("popup");
 let product_details = document.getElementById("product_details");
@@ -10,10 +9,8 @@ let count = 8;
 let no_more_product = false;
 getProducts();
 
-load_more.addEventListener("click", () => {
-  if (!no_more_product) {
-    getProducts();
-  }
+pop_up.addEventListener("click", () => {
+  pop_up.style.display = "none";
 });
 
 function getProducts() {
@@ -34,113 +31,214 @@ function getProducts() {
         no_more_product = true;
       } else {
         result.forEach((product) => {
-         createProductItem(product )
+          createProductItem(product);
         });
       }
     });
 }
 
-function createProductItem(productItem)
-{
-  let productUI = document.createElement("div");
+function createProductItem(product) {
+  const productUI = document.createElement("div");
   productUI.classList.add("product_card");
 
-  productUI.innerHTML = `
-    <img src=${productItem.image} class="product_img" alt="...">
-    <div>
-    <h5 class="product_title">${productItem.title}</h5>
-    <span class="product_price">Rs.${productItem.price}/-</span>
-    </div>
-    <div>
-      <button class="secondaryButton"  onclick="addToCart(${productItem.pid})"  id=${productItem.pid}>Add To Cart</button>
-      <button class="primaryButton"  onclick="viewMore(${productItem.pid})"  id=${productItem.pid}>View More</button>
-    </div>
-    <br>`;
+  const productImg = document.createElement("img");
+  productImg.classList.add("product_img");
+  productImg.src = "../" + product.image;
+
+  const div1 = document.createElement("div");
+
+  const title = document.createElement("p");
+  title.classList.add("product_title");
+  title.innerHTML = product.title;
+  div1.appendChild(title);
+
+  const price = document.createElement("p");
+  price.classList.add("product_price");
+  price.innerHTML = `Rs. ${product.price}/-`;
+  div1.appendChild(price);
+
+  const description = document.createElement("p");
+  description.classList.add("product_desc");
+  description.innerHTML = product.description;
+
+  const div2 = document.createElement("div");
+
+  const addToCart = document.createElement("button");
+  addToCart.classList.add("secondaryButton");
+  addToCart.innerHTML = "Add To Cart";
+  div2.appendChild(addToCart);
+
+  addToCart.addEventListener("click", () => {
+  
+    if (addToCart.innerHTML == "Add To Cart") {
+      fetch("/addToCart", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({ pid: product.pid }),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          if (result["err"]) {
+            alert("Something Went Wrong");
+          } else if (result["data"] == "Not Login") {
+            window.location.href = "./login";
+          } else {
+            addToCart.innerHTML = "Remove From Cart";
+          }
+          console.log(result);
+        });
+    } else if (p.innerHTML == "Remove From Cart") {
+      fetch("/removeFromCart", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({ pid: product.pid }),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          if (result["err"]) {
+            alert("Something Went Wrong");
+          } else {
+            addToCart.innerHTML = "Add To Cart";
+          }
+          console.log(result);
+        });
+    }
+  });
+
+  const viewMore = document.createElement("button");
+  viewMore.classList.add("primaryButton");
+  viewMore.innerHTML = "View More";
+  div2.appendChild(viewMore);
+
+  viewMore.addEventListener("click", () => {
+    pop_up.style.display = "block";
+
+    product_details.innerHTML = "";
+
+    fetch("/product/productDetails", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ id: product.pid }),
+    })
+      .then((response) => response.json())
+      .then((product) => {
+        let img = document.createElement("img");
+        img.src = product.image;
+        img.classList.add("product_img");
+
+        let div = document.createElement("div");
+        div.classList.add("product_card_side");
+
+        const div1 = document.createElement("div");
+
+        const title = document.createElement("p");
+        title.classList.add("product_title");
+        title.innerHTML = product.title;
+        div1.appendChild(title);
+      
+        const price = document.createElement("p");
+        price.classList.add("product_price");
+        price.innerHTML = `Rs. ${product.price}/-`;
+        div1.appendChild(price);
+      
+        const description = document.createElement("p");
+        description.classList.add("product_desc");
+        description.innerHTML = product.description;
+
+        const div2 = document.createElement("div");
+
+        const addToCart = document.createElement("button");
+        addToCart.classList.add("secondaryButton");
+        addToCart.innerHTML = "Add To Cart";
+        div2.appendChild(addToCart);
+      
+        addToCart.addEventListener("click", (event) => {
+            event.stopPropagation()
+          if (addToCart.innerHTML == "Add To Cart") {
+            fetch("/addToCart", {
+              method: "POST",
+              headers: {
+                "Content-type": "application/json;charset=utf-8",
+              },
+              body: JSON.stringify({ pid: product.pid }),
+            })
+              .then((response) => response.json())
+              .then((result) => {
+                if (result["err"]) {
+                  alert("Something Went Wrong");
+                } else if (result["data"] == "Not Login") {
+                  window.location.href = "./login";
+                } else {
+                  addToCart.innerHTML = "Remove From Cart";
+                }
+                console.log(result);
+              });
+          } else if (p.innerHTML == "Remove From Cart") {
+            fetch("/removeFromCart", {
+              method: "POST",
+              headers: {
+                "Content-type": "application/json;charset=utf-8",
+              },
+              body: JSON.stringify({ pid: product.pid }),
+            })
+              .then((response) => response.json())
+              .then((result) => {
+                if (result["err"]) {
+                  alert("Something Went Wrong");
+                } else {
+                  addToCart.innerHTML = "Add To Cart";
+                }
+                console.log(result);
+              });
+          }
+        });
+      
+        const buyNow = document.createElement("button");
+        buyNow.classList.add("primaryButton");
+        buyNow.innerHTML = "Buy Now";
+        div2.appendChild(buyNow);
+
+        buyNow.addEventListener('click',(event)=>{
+          event.stopPropagation();
+          fetch("/addToCart", {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify({ pid: product.pid }),
+          })
+            .then((response) => response.json())
+            .then((result) => {
+              if (result["err"]) {
+                alert("Something Went Wrong");
+              } else if (result["data"] == "Not Login") {
+                window.location.href = "./login";
+              } else {
+                window.location.href = "./cart";
+              }
+              console.log(result);
+            });
+
+        })
+      
+        div.append(div1);
+        div.append(div2);
+
+        product_details.append(img);
+        product_details.append(div);
+      });
+  });
+
+  productUI.appendChild(productImg);
+  productUI.appendChild(div1);
+  productUI.appendChild(description);
+  productUI.appendChild(div2);
+  productUI.appendChild(document.createElement("br"));
   items.appendChild(productUI);
 }
-
-function addToCart(id) {
-  let p = document.getElementById(id);
-
-  if (p.innerHTML == "Add To Cart") {
-    fetch("/addToCart", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify({ pid: id }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result["err"]) {
-          alert("Something Went Wrong");
-        } else if(result['data']=='Not Login'){
-          window.location.href='./login'
-        }else {
-          p.innerHTML = "Remove From Cart";
-        }
-        console.log(result);
-      });
-  } else if (p.innerHTML == "Remove From Cart") {
-    fetch("/removeFromCart", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify({ pid: id }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result["err"]) {
-          alert("Something Went Wrong");
-        } else {
-          p.innerHTML = "Add To Cart";
-        }
-        console.log(result);
-      });
-  }
-}
-
-function viewMore(id) {
-  pop_up.style.display = "block";
-
-  product_details.innerHTML = "";
-
-  fetch("/product/productDetails", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ id: id }),
-  })
-    .then((response) => response.json())
-    .then((product) => {
-      let img = document.createElement("img");
-      img.src = product.image;
-      img.classList.add("product_img");
-      let div = document.createElement("div");
-      div.classList.add("product_card_side");
-      let div1 = document.createElement("div");
-      let div2 = document.createElement("div");
-      div1.innerHTML = `
-          <div>
-              <h5 class="product_title">${product.title}</h5>
-              <h5 class="product_desc">iPhone 9</h5>
-
-              <span class="product_price">Rs.${product.price}/-</span>
-          </div>`;
-
-      div2.innerHTML = `  <div>
-      <button class="secondaryButton"  onclick="addToCart(${product.pid})"  id=${product.pid}>Add To Cart</button>
-      <button class="primaryButton"    id=${product.pid}>Buy Now</button>
-    </div>`;
-      div.append(div1);
-      div.append(div2);
-      product_details.append(img);
-      product_details.append(div);
-    });
-}
-
-pop_up.addEventListener("click", () => {
-  pop_up.style.display = "none";
-});
